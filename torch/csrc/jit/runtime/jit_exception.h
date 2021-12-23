c89823763451276031375d2a9b62955422c60897
Shunting Zhang <shunting@fb.com>
2021-12-22 17:52:47 -0800

    Make TorchScript Preserve Fully Qualified Class Name for Python Exceptions
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/70339
    
    When a python program is translated to TorchScript, the python exception type is dropped. This makes users's life hard when they need to categorize errors based more than only exception message.
    
    Here we make the change so when we raise a python exception, we record the fully qualified class name for the exception. Later on when the TorchScript is interpreted, a special exception CustomJITException is thrown. User can get the python class name from CustomJITException::getPythonClassName .
    
    Note that, this diff does not customize the mapping from C++ exception to Python exception. It's left to the users to do whatever mapping they want.
    
    Code under scripts/shunting are just my own experimental code. I can split them out if requested.
    ghstack-source-id: 146183134
    
    Differential Revision: [D33282878](https://our.internmc.facebook.com/intern/diff/D33282878/)
    
    **NOTE FOR REVIEWERS**: This PR has internal Facebook specific changes or comments, please review them on [Phabricator](https://our.internmc.facebook.com/intern/diff/D33282878/)!

--- torch/csrc/jit/runtime/jit_exception.h
+++ torch/csrc/jit/runtime/jit_exception.h
@@ -3,12 +3,26 @@
 #include <stdexcept>
 
 #include <torch/csrc/Export.h>
+#include <string>
 
 namespace torch {
 namespace jit {
 
 struct TORCH_API JITException : public std::runtime_error {
   explicit JITException(const std::string& msg);
+};
+
+struct TORCH_API CustomJITException : public JITException {
+  explicit CustomJITException(
+      const std::string& msg,
+      std::string python_class_name);
+
+  const std::string& getPythonClassName() const {
+    return python_class_name_;
+  }
+
+ private:
+  std::string python_class_name_;
 };
 
 } // namespace jit
